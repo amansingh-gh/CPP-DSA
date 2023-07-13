5ec2365696f89c9438d24abed7e50824690a0e88
Aman Singh <112749591+amansingh-gh@users.noreply.github.com>
2023-07-13 18:27:28 +0530

    more

--- more_arr.cpp
+++ more_arr.cpp
@@ -63,5 +63,27 @@ int main()
         cout << r_arr[i] << " ";
     }
 
+
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int size = 10;
+
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+        //  For printing reverse of an array;
+        for (int i = 0; i < size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+
+
+    return 0;
+
     return 0;
 }
