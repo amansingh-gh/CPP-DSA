5612e3723878db582e7f775b26f5ae0514365079
Aman Kumar Singh <112749591+amansingh-gh@users.noreply.github.com>
2023-09-05 19:20:55 +0530

    Update more_ptr.cpp

--- Pointer/more_ptr.cpp
+++ Pointer/more_ptr.cpp
@@ -3,37 +3,36 @@ using namespace std;
 
 int main()
 {
-    // int arr[4] = {12, 14, 75, 89};
-    // cout << "The address of arr is : " << arr << endl;
-    // cout << "The value of *arr is : " << *arr << endl;
-    // cout << "The value of arr+1 is : " << *(arr + 1) << endl;
-    // cout << "The address of arr+1 is : " << (arr + 1) << endl;
+    int arr[4] = {12, 14, 75, 89};
+    cout << "The address of arr is : " << arr << endl;
+    cout << "The value of *arr is : " << *arr << endl;
+    cout << "The value of arr+1 is : " << *(arr + 1) << endl;
+    cout << "The address of arr+1 is : " << (arr + 1) << endl;
 
-    // cout << *(arr + 3) << endl;
-    // cout << *(arr + 1) << endl;
+    cout << *(arr + 3) << endl;
+    cout << *(arr + 1) << endl;
 
-    // cout << arr[0] << endl;
-    // cout << 0 [arr] << endl;
+    cout << arr[0] << endl;
+    cout << 0 [arr] << endl;
 
-    // int arrr[10];
-    // cout << "The size of arrr is : " << sizeof(arrr) << endl;
+    int arrr[10];
+    cout << "The size of arrr is : " << sizeof(arrr) << endl;
 
     // char array
 
-    // int a[5] = {1,2,3,4,5};
-    // cout << &a[0] << endl;
+    int a[5] = {1, 2, 3, 4, 5};
+    cout << &a[0] << endl;
 
-    // char ch[10] = "Honey";
-    // char *c = ch;
+    char ch[10] = "Honey";
+    char *c = ch;
 
-    // cout << *c << endl;
-    // cout << &ch << endl;
-    // cout << &ch[0] << endl;
+    cout << *c << endl;
+    cout << &ch << endl;
+    cout << &ch[0] << endl;
 
     char cch = 'k';
     char *cpptrr = &cch;
     cout << cpptrr << endl;
-
 
     return 0;
 }
